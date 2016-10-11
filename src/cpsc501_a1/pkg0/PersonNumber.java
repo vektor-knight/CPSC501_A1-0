c673405c356f045cf772b1e7646fc20036c4ca32
shshunda <shshunda@ZONE05-WC.cs.ucalgary.ca>
2016-10-11 11:26:48 -0600

    Initialize project with test suites for original classes.

---
+++ src/cpsc501_a1/pkg0/PersonNumber.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cpsc501_a1.pkg0;
+
+/**
+ *
+ * @author shshunda
+ */
+public class PersonNumber {
+  private String name;
+  private String officeAreaCode;
+  private String officeNumber;
+
+  public String getName() {
+    return name;
+  }
+  public String getTelephoneNumber() {
+    return ("(" + officeAreaCode + ") " + officeNumber);
+  }
+  public String getOfficeAreaCode() {
+    return officeAreaCode;
+  }
+  public void setOfficeAreaCode(String arg) {
+    officeAreaCode = arg;
+  }
+  public String getOfficeNumber() {
+    return officeNumber;
+  }
+  public void setOfficeNumber(String arg) {
+    officeNumber = arg;
+}
+}
