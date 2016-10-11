2e526029fa232a2642012546dc91ee3b0e45204e
shshunda <shshunda@ZONE05-WC.cs.ucalgary.ca>
2016-10-11 11:43:36 -0600

    Refactor 1: "Rename Class" | PersonNumber.java -> Person.java

---
+++ src/cpsc501_a1/pkg0/Person.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cpsc501_a1.pkg0;
+
+/**
+ *
+ * @author shshunda
+ */
+
+// Applying first refactoring: "Rename Class"
+// Name of file changed from PersonClass.java -> Person.java
+public class Person {
+  private String name = "Bob";
+  private String officeAreaCode = "403";
+  private String officeNumber = "2071307";
+
+  public String getName() {
+    return name;
+  }
+  public String getTelephoneNumber() {
+    return ("(" + officeAreaCode + ") " + officeNumber);
+  }
+  public String getOfficeAreaCode() {
+    return officeAreaCode;
+  }
+  public void setOfficeAreaCode(String arg) {
+    officeAreaCode = arg;
+  }
+  public String getOfficeNumber() {
+    return officeNumber;
+  }
+  public void setOfficeNumber(String arg) {
+    officeNumber = arg;
+}
+}
